3dbc6b1b813a8036fdf0f7ed043ae4f0342b7253
jaber20106 <azharulislamjaber20106@gmail.com>
2026-03-06 23:41:49 +0600

    02-Components

---
+++ 02-Components/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Azharul islam</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, quis?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default card;
---
+++ 02-Components/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <h1>Navbar</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, libero?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
